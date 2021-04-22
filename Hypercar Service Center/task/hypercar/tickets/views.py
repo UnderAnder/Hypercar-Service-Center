841ecd38cd414b75353aadfb80a39566d8a35bae
Under Ander <andryushin.alexander@gmail.com>
2021-04-22 19:44:48 +0700

    Stage 2/5: Customer menu

--- Hypercar Service Center/task/hypercar/tickets/views.py
+++ Hypercar Service Center/task/hypercar/tickets/views.py
@@ -1,7 +1,14 @@
 from django.views import View
 from django.http.response import HttpResponse
+from django.shortcuts import render
 
 
 class WelcomeView(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse('<h2>Welcome to the Hypercar Service!</h2>')
+
+
+class MenuView(View):
+    def get(self, request, *args, **kwargs):
+        services = {'change_oil': 'Change oil', 'inflate_tires': 'Inflate tires', 'diagnostic': 'Get diagnostic test'}
+        return render(request, 'menu.html', context={'services': services})
